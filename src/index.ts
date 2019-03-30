ed33d2cbb759b2cf193d89775bbe7fe15c480986
Michael Moore <michael.moore@contrastsecurity.com>
2019-03-30 19:35:08 -0400

    remove class

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,20 @@
 import { gridFactory } from './grid'
 import { carveMaze } from './carve-maze'
 
-class MazeGen {
-  private rows: number
-  private cols: number
-  constructor(rows: number, cols: number) {
-    if (typeof rows !== 'number') {
-      throw new Error('rows is required')
-    }
-    if (typeof cols !== 'number') {
-      throw new Error('cols is required')
-    }
-    this.rows = rows
-    this.cols = cols
-  }
-  public generate = () => {
-    const grid = gridFactory(this.rows, this.cols)
-    carveMaze(grid)
-    return grid
-  }
-}
-
 export const mazeGenerator = (rows: number, cols: number) => {
-  const mazeGen = new MazeGen(rows, cols)
-  return mazeGen.generate()
+  if (typeof rows !== 'number') {
+    throw new Error('rows is required')
+  }
+  if (rows < 0) {
+    throw new Error('rows must be a positive integer')
+  }
+  if (typeof cols !== 'number') {
+    throw new Error('cols is required')
+  }
+  if (cols < 0) {
+    throw new Error('cols must be a positive integer')
+  }
+  const grid = gridFactory(rows, cols)
+  carveMaze(grid)
+  return grid
 }
