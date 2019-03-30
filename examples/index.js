2eb630e9939433574c64e5f9c1d383e92528cd11
Michael Moore <michael.moore@contrastsecurity.com>
2019-03-30 19:43:40 -0400

    use object for params

--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,3 @@
-const mazegen = require('../dist/index')
-const grid = mazegen.mazeGenerator(10, 10)
+const maze = require('../dist/index')
+const grid = maze.mazeGenerator({ rows: 10, columns: 10 })
 console.log(grid)
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,34 @@
 import { gridFactory } from './grid'
 import { carveMaze } from './carve-maze'
 
-export const mazeGenerator = (rows: number, cols: number) => {
+interface Params {
+  readonly rows: number
+  readonly columns: number
+}
+
+const isObject = (obj: any) => {
+  var type = typeof obj
+  return type === 'function' || (type === 'object' && !!obj)
+}
+
+export const mazeGenerator = (params: Params) => {
+  if (Array.isArray(params) || !isObject(params)) {
+    throw new Error('params must be an object')
+  }
+  const { rows, columns } = params
   if (typeof rows !== 'number') {
     throw new Error('rows is required')
   }
   if (rows < 0) {
     throw new Error('rows must be a positive integer')
   }
-  if (typeof cols !== 'number') {
-    throw new Error('cols is required')
+  if (typeof columns !== 'number') {
+    throw new Error('columns is required')
   }
-  if (cols < 0) {
-    throw new Error('cols must be a positive integer')
+  if (columns < 0) {
+    throw new Error('columns must be a positive integer')
   }
-  const grid = gridFactory(rows, cols)
+  const grid = gridFactory(rows, columns)
   carveMaze(grid)
   return grid
 }
