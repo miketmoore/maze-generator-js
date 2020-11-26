66dd55675bc08b3c0cf59248c99217f6b367a14e
Michael Moore <michael.moore@contrastsecurity.com>
2020-11-26 10:53:51 -0500

    Fix bug - recursive runs both strategies
    The switch statement falls through from recursive to iterative. Add
    break keywords to avoid this.

--- src/carve-maze.ts
+++ src/carve-maze.ts
@@ -4,17 +4,17 @@ import { carveIterative } from './carve-iterative'
 
 export type Strategy = 'recursive-backtracking' | 'iterative'
 
-export function carveMaze(
-  rows: number,
-  columns: number,
-  strategy: Strategy = 'recursive-backtracking'
-) {
+export function carveMaze(rows: number, columns: number, strategy: Strategy) {
   const grid = gridFactory(rows, columns)
   switch (strategy) {
     case 'recursive-backtracking':
       carveRecursiveBacktracking(grid)
+      break
     case 'iterative':
       carveIterative(grid)
+      break
+    default:
+      throw new Error(`unexpected strategy: '${strategy}'`)
   }
   return grid
 }
