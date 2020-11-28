72cd32ddbc375e0d325e7c8f02e3571730fae0bf
Michael Moore <michael.moore@contrastsecurity.com>
2020-11-28 17:06:17 -0500

    rename snapshot file

--- examples/for-blog.js
+++ examples/for-blog.js
@@ -2,4 +2,4 @@ const heapdump = require('heapdump')
 const maze = require('../dist/index')
 const size = 500
 const grid = maze.mazeGenerator({ rows: size, columns: size }, 'iterative')
-heapdump.writeSnapshot('first-snapshot.heapsnapshot')
+heapdump.writeSnapshot('snapshot-01.heapsnapshot')
