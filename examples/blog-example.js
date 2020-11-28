616284371c90e07fcd0299a48d0271a669d71e49
Michael Moore <michael.moore@contrastsecurity.com>
2020-11-28 17:13:19 -0500

    Write script to trigger out of memory error

---
+++ examples/blog-example.js
@@ -0,0 +1,6 @@
+const maze = require('../dist/index')
+/*
+ * This is the minimum size (at least for a square grid) that will trigger an out of memory error
+ */
+const size = 1016
+const grid = maze.mazeGenerator({ rows: size, columns: size }, 'iterative')
