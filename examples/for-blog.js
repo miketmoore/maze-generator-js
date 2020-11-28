20e10782a83141d14b3aa95b2ed405686cec0533
Michael Moore <michael.moore@contrastsecurity.com>
2020-11-28 14:47:24 -0500

    use heapdump

--- examples/for-blog.js
+++ examples/for-blog.js
@@ -1,3 +1,5 @@
+const heapdump = require('heapdump')
 const maze = require('../dist/index')
-const size = 1016
+const size = 500
 const grid = maze.mazeGenerator({ rows: size, columns: size }, 'iterative')
+heapdump.writeSnapshot('snapshot-00.heapsnapshot')
