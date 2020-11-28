bed37efafd9006cb7dda634ba186ea17088b32db
Michael Moore <michael.moore@contrastsecurity.com>
2020-11-28 17:17:22 -0500

    Reduce grid size to 500x500

--- examples/blog-example.js
+++ examples/blog-example.js
@@ -3,6 +3,6 @@ const maze = require('../dist/index')
 /*
  * This is the minimum size (at least for a square grid) that will trigger an out of memory error
  */
-const size = 1016
+const size = 500
 const grid = maze.mazeGenerator({ rows: size, columns: size }, 'iterative')
 heapdump.writeSnapshot('snapshot-01.heapsnapshot')
