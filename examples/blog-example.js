c322a781a2f4d8aea432080fcecafc82fe7be527
Michael Moore <michael.moore@contrastsecurity.com>
2020-11-28 17:16:41 -0500

    Create heap snapshot in example script

--- examples/blog-example.js
+++ examples/blog-example.js
@@ -1,6 +1,8 @@
+const heapdump = require('heapdump')
 const maze = require('../dist/index')
 /*
  * This is the minimum size (at least for a square grid) that will trigger an out of memory error
  */
 const size = 1016
 const grid = maze.mazeGenerator({ rows: size, columns: size }, 'iterative')
+heapdump.writeSnapshot('snapshot-01.heapsnapshot')
