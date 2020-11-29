182577429ded99e10b905566746f0b40228fc2c4
Michael Moore <michael.moore@contrastsecurity.com>
2020-11-28 22:12:51 -0500

    update snapshot file name

--- examples/blog-example.js
+++ examples/blog-example.js
@@ -5,4 +5,4 @@ const maze = require('../dist/index')
  */
 const size = 500
 const grid = maze.mazeGenerator({ rows: size, columns: size }, 'iterative')
-heapdump.writeSnapshot('snapshot-01.heapsnapshot')
+heapdump.writeSnapshot('snapshot-02.heapsnapshot')
