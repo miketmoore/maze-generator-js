ce9dfb3625936ba9706f6c6dd31fd58f69057716
Michael Moore <michael.moore@contrastsecurity.com>
2020-11-29 23:07:18 -0500

    bump size to 1500

--- examples/blog-example.js
+++ examples/blog-example.js
@@ -2,5 +2,5 @@ const maze = require('../dist/index')
 /*
  * This is the minimum size (at least for a square grid) that will trigger an out of memory error
  */
-const size = 1016
+const size = 1500
 const grid = maze.mazeGenerator({ rows: size, columns: size }, 'iterative')
